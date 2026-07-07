fd4bd14b21fb9f40c790c719bfb7a28dd31f922e
minenko-mykola <minenkomykola444@gmail.com>
2026-07-07 11:58:43 +0300

    Refactored search-indexer /index-all endpoint

--- services/search-indexer/actions/indexProduct.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-
-export async function indexProduct(req : express.Request, res : express.Response)
-{
-    res.status(200).json({
-        message : "Indexed product successfully"
-    })
-}
