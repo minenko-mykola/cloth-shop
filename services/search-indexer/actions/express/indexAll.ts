1c2a8e068e8c0d337ebf947b889be45b8138f9ea
minenko-mykola <minenkomykola444@gmail.com>
2026-07-28 19:35:11 +0300

    Minor changes(commented MongoDB operations)

--- services/search-indexer/actions/express/indexAll.ts
+++ services/search-indexer/actions/express/indexAll.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import {ProductModels} from "../../entities/sequelize";
-import {indexBatch, getBatch, writeLog, updateLog} from "../internal";
+import {indexBatch, getBatch} from "../internal";
 import {uuidv7} from "uuidv7";
 
 dotenv.config({ path: "envs/.env.sequelize", override: false });
@@ -13,13 +13,13 @@ export async function indexAll(req : express.Request, res : express.Response)
 
     try
     {
-        const log = await writeLog({
-            name: "search-indexer",
-            operationId: uuidv7(),
-            status: "started"
-        })
+        // const log = await writeLog({
+        //     name: "search-indexer",
+        //     operationId: uuidv7(),
+        //     status: "started"
+        // })
 
-        log_id = log._id.toString();
+        // log_id = log._id.toString();
 
         res.status(202).json({
             message : "Started indexing",
@@ -42,20 +42,20 @@ export async function indexAll(req : express.Request, res : express.Response)
         }
         while (batch.length !== 0)
 
-        const result = await updateLog({
-            id : log_id,
-            status : "completed"
-        })
+        // const result = await updateLog({
+        //     id : log_id,
+        //     status : "completed"
+        // })
 
     }
     catch(err)
     {
         console.log(`[Search Indexer] : Error while indexing all products:${err}`)
 
-        const result = await updateLog({
-            id : log_id,
-            status : "failed"
-        })
+        // const result = await updateLog({
+        //     id : log_id,
+        //     status : "failed"
+        // })
 
         res.status(500).json({
             message : `Error while indexing all products:${err}`
